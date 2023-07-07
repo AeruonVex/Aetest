94c5f1038dc5132098102d606646a99787c127ef
Salomón Dávalos <88594657+AeruonVex@users.noreply.github.com>
2023-07-07 19:16:34 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const venom = require('venom-bot');
+
+const PREFIX = '#';
+const OWNER_NUMBER = '595994422882';
+
+venom
+  .create()
+  .then((client) => startBot(client))
+  .catch((error) => console.log('Error al iniciar el bot:', error));
+
+function startBot(client) {
+  client.onMessage(async (message) => {
+    // Obtener información del mensaje
+    const { from, body } = message;
+
+    // Verificar si el mensaje es un comando válido
+    if (body.startsWith(PREFIX)) {
+      const command = body.slice(1); // Eliminar el prefijo del comando
+
+      // Comando de menú
+      if (command === 'menu') {
+        await client.sendText(from, 'Hola! Este es el menú principal');
+      }
+
+      // Comando de dueño
+      if (command === 'owner' && from.endsWith(OWNER_NUMBER)) {
+        await client.sendText(from, 'Hola creador');
+      }
+    }
+  });
+
+  // Escanear código QR
+  client
+    .onStateChange((state) => {
+      if (state.qrCode) {
+        // Mostrar el código QR y esperar a que se escanee
+        console.log('Escanea el código QR:');
+        console.log(state.qrCode);
+      }
+    })
+    .catch((error) => console.log('Error al escanear el código QR:', error));
+    }
+    
